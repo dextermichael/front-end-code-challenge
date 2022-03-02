7edf9b8e028cfbe2fabf8724119901c26797146a
Dexter Hennington <henmgdexter@gmail.com>
2022-03-02 17:19:19 -0500

    build main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import styles from './Index.module.scss';
+import Head from "next/head";
+import styles from "./Index.module.scss";
 
 export const IndexPage = () => {
   return (
@@ -11,13 +11,34 @@ export const IndexPage = () => {
         <link rel="icon" href="favicon.ico" />
       </Head>
       <div className={styles.container}>
-        <div>Rehash Code Challenge</div>
-        <div>your name</div>
-        <div>WebSockets message here</div>
-        <div>RESTful API query results</div>
+        <h1 className={styles.title}>Rehash Code Challenge</h1>
+        <h2 className={styles.name}>Dexter Hennington</h2>
+        <h3 className={styles.subTitle}>Server Status</h3>
+        <div className={styles.serverStatus}>Plutonium</div>
+        <h3 className={styles.subTitle}>Database Entries</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Gender</th>
+              <th>Email</th>
+              <th>IP Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jeanette</td>
+              <td>Penddreth</td>
+              <td>Female</td>
+              <td>jpenddreth@census.gov</td>
+              <td>26.58.193.2</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default IndexPage;
